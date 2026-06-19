de5b39fc3b2ea17ee8d274a384a07c3275a6c031
LiamCz1 <officeoflias@gmail.com>
2026-06-19 08:15:27 -0500

    v10

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.getElementById('shop-button');
-
-  if (!button) {
-    return;
+  if (button) {
+    button.addEventListener('click', () => {
+      window.location.href = 'product.html';
+    });
   }
 
-  button.addEventListener('click', () => {
-    window.location.href = 'product.html';
-  });
+  const button1 = document.getElementById('shop-button1');
+  if (button1) {
+    button1.addEventListener('click', () => {
+      window.location.href = 'lessons.html';
+    });
+  }
 });
